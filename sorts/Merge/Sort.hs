8f1cb93ac192b6108f3ef3d9fa143174a8d36d3d
juleari <juleari@yandex.ru>
2017-03-04 15:49:27 +0300

    -helper
    haskell

--- sorts/Merge/Sort.hs
+++ sorts/Merge/Sort.hs
@@ -1,12 +1,9 @@
 module Merge.Sort where
 -- Merge sort algorithm.
-merge_sort :: [a] -> (a -> a -> Bool) -> [a]
-merge_sort xs cmp = helper cmp xs
-
-helper :: (a -> a -> Bool) -> [a] -> [a]
-helper cmp []  = []
-helper cmp [x] = [x]
-helper cmp xs  = merge cmp (helper cmp (left xs)) (helper cmp (right xs))
+merge_sort :: (a -> a -> Bool) -> [a] -> [a]
+merge_sort cmp []  = []
+merge_sort cmp [x] = [x]
+merge_sort cmp xs  = merge cmp (merge_sort cmp (left xs)) (merge_sort cmp (right xs))
 
 merge :: (a -> a -> Bool) -> [a] -> [a] -> [a]
 merge cmp [] rs = rs
@@ -18,4 +15,4 @@ half_length xs = ceiling (fromIntegral (length xs) / 2)
 left  xs = take (half_length xs) xs
 right xs = take (half_length xs) (drop (half_length xs) xs)
 
-sort = merge_sort
+sort xs cmp = merge_sort cmp xs
